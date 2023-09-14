8b7ef66a91f5f2fc4d31cf6b7f6ebfb9c7f7b944
Tim Dirks <tim.dirks@hotmail.com>
2023-09-14 15:14:02 +0200

    Created roman matrix array

--- 5/solution.js
+++ 5/solution.js
@@ -1,1 +1,18 @@
+// To convert a number to roman numerals I'll be using a dictionary table which can be looped over.
+const romanMatrix = [
+    [1000, 'M'],
+    [900, 'CM'],
+    [500, 'D'],
+    [400, 'CD'],
+    [100, 'C'],
+    [90, 'XC'],
+    [50, 'L'],
+    [40, 'XL'],
+    [10, 'X'],
+    [9, 'IX'],
+    [5, 'V'],
+    [4, 'IV'],
+    [1, 'I'],
+];
+
 console.log('Output');
